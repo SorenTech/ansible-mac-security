010b42f1944edb7f8e52c60ff43c5316aefe58a7
Alex Floyd Marshall <apmarshall@soren.tech>
2020-03-12 19:55:14 -0400

    Fix: don't start next line unless previous line executes correctly

--- install.sh
+++ install.sh
@@ -9,35 +9,37 @@
 # Ask for the administrator password upfront
 sudo -v
 
+# To-Do: Install X-Code Tools First or else this whole thing derails
+
 # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # Install pip and ansible if they aren't already on the system 
 if [ ! command -v pip >/dev/null 2>&1 ]; then
     sudo easy_install pip
-fi
+fi &&
 
 if [ ! command -v ansible >/dev/null 2>&1 ]; then
     sudo pip install ansible
-fi
+fi &&
 
 # Add ansible.cfg to pick up roles path.
 # "{ echo '[defaults]'; echo 'roles_path = ../'; } >> ansible.cfg"
 
 # Add a hosts file.
-sudo mkdir -p /etc/ansible
-sudo touch /etc/ansible/hosts
-echo -e '[local]\nlocalhost ansible_connection=local' | sudo tee -a /etc/ansible/hosts >/dev/null
+sudo mkdir -p /etc/ansible &&
+sudo touch /etc/ansible/hosts &&
+echo '[local]\nlocalhost ansible_connection=local' | sudo tee -a /etc/ansible/hosts >/dev/null &&
 
 # clone repo
 git clone https://github.com/SorenTech/ansible-mac-security.git ~/.baseline
-cd ~/.baseline
+cd ~/.baseline &&
 
 # install requirements
-ansible-galaxy install -r requirements.yml
+ansible-galaxy install -r requirements.yml &&
 
 # check syntax
-ansible-playbook main.yml --syntax-check
+ansible-playbook main.yml --syntax-check &&
 
 # run playbook
 ansible-playbook --extra-vars '{\"configure_sudoers\":\"false\"}' main.yml
