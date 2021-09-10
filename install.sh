117e691335a9920e1d6205d8fe126e4cf3f631e8
Alex Floyd Marshall <apmarshall@soren.tech>
2021-09-10 15:49:45 -0400

    Latest updates from Develop (#4)
    * Fix: don't start next line unless previous line executes correctly
    
    * Fixes to DNS configs
    
    * Updating the user agent to be more generic (chrome and windows)
    
    * Update: use new homebrew role from geerlingguy collection
    
    * Fixes to install script
    
    1. Remove installation of python and pip: this will need to be done
    manually as a prereq because its different packages on different chip
    architectures
    
    2. Some minor shell syntax fixes that should make the script work
    better
    
    * Package list changes
    
    1. Remove pivoxy: DNS black whole is taking care of this functionality
    2. Remove python: being preinstalled before running script
    3. Add firefox, chrome, and vivaldi so we can configure them with
    security settings
    
    * Removing users task and privoxy related steps and files
    
    * Scripts and Plists
    
    * Adding support for hostsupdater launch daemon
    
    * Task to start morningjoe Launch Daemon
    
    * Firefox policies.json
    
    * Should automatically upgrade casks, no need for an extra command
    
    * Configure firefox with policies.json
    
    * Add morning joe to list of tasks to run
    
    * CI Workflow for testing playbook
    
    * Fix: install ansible-core
    
    * Fix: Requirements installation syntax
    
    * Updating Ansible config and inventory
    
    * Fix: skip the homebrew portion when testing so we can complete our tests
    
    * Fix: spelling
    
    * Don't use shell scripts, perform jobs straight from launchd plists
    
    * Fix: sudo to install hosts package
    
    * Fix: use shell so we can sudo on git clone
    
    * Elevate status for some more commands

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Simple Install Script to Set Up Security Baseline on a New Mac with One Command Using Curl
+# Prerequisites: Install Python for your particular Mac Architecture (whether that is intel or M1)
 # What it does:
-# - Install pip (if needed) and then install Ansible with pip
+# - Install Ansible with pip
 # - Clone Ansible Playbook with git
 # - Run Ansible Playbook to Configure New Mac (including installing xcode tools and homebrew)
 
@@ -12,32 +13,25 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install pip and ansible if they aren't already on the system 
-if [ ! command -v pip >/dev/null 2>&1 ]; then
-    sudo easy_install pip
-fi
-
-if [ ! command -v ansible >/dev/null 2>&1 ]; then
-    sudo pip install ansible
-fi
+sudo pip install ansible-core &&
 
 # Add ansible.cfg to pick up roles path.
-# "{ echo '[defaults]'; echo 'roles_path = ../'; } >> ansible.cfg"
+echo "[defaults]\n roles_path = ../" >> ansible.cfg
 
 # Add a hosts file.
-sudo mkdir -p /etc/ansible
-sudo touch /etc/ansible/hosts
-echo -e '[local]\nlocalhost ansible_connection=local' | sudo tee -a /etc/ansible/hosts >/dev/null
+sudo mkdir -p /etc/ansible &&
+sudo touch /etc/ansible/hosts &&
+echo "[local]\n localhost ansible_connection=local" | sudo tee -a /etc/ansible/hosts >/dev/null &&
 
 # clone repo
-git clone https://github.com/SorenTech/ansible-mac-security.git ~/.baseline
-cd ~/.baseline
+git clone https://github.com/SorenTech/ansible-mac-security.git ~/.baseline &&
+cd ~/.baseline &&
 
 # install requirements
-ansible-galaxy install -r requirements.yml
+ansible-galaxy install -r requirements.yml &&
 
 # check syntax
-ansible-playbook main.yml --syntax-check
+ansible-playbook main.yml --syntax-check &&
 
 # run playbook
 ansible-playbook --extra-vars '{\"configure_sudoers\":\"false\"}' main.yml
