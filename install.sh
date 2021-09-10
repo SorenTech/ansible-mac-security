b8bfcb0b41739cc3fddd88cdf09c575d5769f8ff
Alex Floyd Marshall <apmarshall@soren.tech>
2021-09-09 20:40:03 -0400

    Fixes to install script
    1. Remove installation of python and pip: this will need to be done
    manually as a prereq because its different packages on different chip
    architectures
    
    2. Some minor shell syntax fixes that should make the script work
    better

--- install.sh
+++ install.sh
@@ -1,38 +1,30 @@
 #!/bin/bash
 
 # Simple Install Script to Set Up Security Baseline on a New Mac with One Command Using Curl
+# Prerequisites: Install Python for your particular Mac Architecture (whether that is intel or M1)
 # What it does:
-# - Install pip (if needed) and then install Ansible with pip
+# - Install Ansible with pip
 # - Clone Ansible Playbook with git
 # - Run Ansible Playbook to Configure New Mac (including installing xcode tools and homebrew)
 
 # Ask for the administrator password upfront
 sudo -v
 
-# To-Do: Install X-Code Tools First or else this whole thing derails
-
 # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Install pip and ansible if they aren't already on the system 
-if [ ! command -v pip >/dev/null 2>&1 ]; then
-    sudo easy_install pip
-fi &&
-
-if [ ! command -v ansible >/dev/null 2>&1 ]; then
-    sudo pip install ansible
-fi &&
+sudo pip install ansible &&
 
 # Add ansible.cfg to pick up roles path.
-# "{ echo '[defaults]'; echo 'roles_path = ../'; } >> ansible.cfg"
+echo "[defaults]\n roles_path = ../" >> ansible.cfg
 
 # Add a hosts file.
 sudo mkdir -p /etc/ansible &&
 sudo touch /etc/ansible/hosts &&
-echo '[local]\nlocalhost ansible_connection=local' | sudo tee -a /etc/ansible/hosts >/dev/null &&
+echo "[local]\n localhost ansible_connection=local" | sudo tee -a /etc/ansible/hosts >/dev/null &&
 
 # clone repo
-git clone https://github.com/SorenTech/ansible-mac-security.git ~/.baseline
+git clone https://github.com/SorenTech/ansible-mac-security.git ~/.baseline &&
 cd ~/.baseline &&
 
 # install requirements
