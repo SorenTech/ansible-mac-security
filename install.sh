8123e1d36a9fa73bdd78e6dafa5351bcdcbb391f
Alex Floyd Marshall <apmarshall@soren.tech>
2021-09-10 13:03:40 -0400

    Fix: install ansible-core

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo pip install ansible &&
+sudo pip install ansible-core &&
 
 # Add ansible.cfg to pick up roles path.
 echo "[defaults]\n roles_path = ../" >> ansible.cfg
