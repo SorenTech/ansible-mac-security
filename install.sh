b8bed2e70a9d4104bf866f903dbaf350590d4074
Alex Floyd Marshall <apmarshall@soren.tech>
2023-02-16 03:26:14 +0000

    Attempted rewrite of the install script

--- install.sh
+++ install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
+set -e
+set -u
+set -x
+
 # Simple Install Script to Set Up Security Baseline on a New Mac with One Command Using Curl
 # Prerequisites: Install Python for your particular Mac Architecture (whether that is intel or M1)
 # What it does:
-# - Install Ansible with pip
-# - Clone Ansible Playbook with git
+# - Installs Homebrew if not present
+# - Installs and Configures Ansible if not present
+# - Clone/Update Ansible Playbook with git
 # - Run Ansible Playbook to Configure New Mac (including installing xcode tools and homebrew)
 
 # Ask for the administrator password upfront
@@ -13,25 +18,52 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `install.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-sudo pip install ansible-core &&
+should_install_homebrew() {
+  ! [[ -e "/usr/local/bin/brew" ]] || ! [[ -e "/opt/homebrew/bin/brew" ]]
+}
 
-# Add ansible.cfg to pick up roles path.
-echo "[defaults]\n roles_path = ../" >> ansible.cfg
+should_install_ansible() {
+  ! [[ ansible --version ]]
+}
 
-# Add a hosts file.
-sudo mkdir -p /etc/ansible &&
-sudo touch /etc/ansible/hosts &&
-echo "[local]\n localhost ansible_connection=local" | sudo tee -a /etc/ansible/hosts >/dev/null &&
+should_clone_repo() {
+  ! [[ -e "~/.baseline/.git" ]]
+}
 
-# clone repo
-git clone https://github.com/SorenTech/ansible-mac-security.git ~/.baseline &&
-cd ~/.baseline &&
+should_configure_ansible() {
+  ! [[ -e "/etc/ansible/ansible.cfg" ]]
+}
 
-# install requirements
-ansible-galaxy install -r requirements.yml &&
+if should_install_homebrew
+then
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
-# check syntax
-ansible-playbook main.yml --syntax-check &&
+if should_install_ansible
+then
+  brew install ansible
+fi
 
-# run playbook
-ansible-playbook --extra-vars '{\"configure_sudoers\":\"false\"}' main.yml
+if should_clone_repo
+then
+  git clone https://github.com/SorenTech/ansible-mac-security.git ~/.baseline
+else
+  cd ~/.baseline && git pull
+fi
+
+if should_configure_ansible
+then
+  cp ~/.baseline/ansible.cfg /etc/ansible/ansible.cfg
+fi
+
+cd ~/.baseline && \
+  echo "Installing playbook requirements" && \
+  ansible-galaxy install -r requirements.yml && \
+  echo "Testing playbook syntax" && \
+  ansible-playbook main.yml -i inventory --syntax-check && \
+  echo "Running playbook" && \
+  ansible-playbook main.yml -i inventory --extra-vars '{\"configure_sudoers\":\"false\"}' && \
+  echo "Playbook completed"
+  
+exit 0
+  
